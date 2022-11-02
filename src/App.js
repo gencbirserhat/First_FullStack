ea6f34757707c95c305778cd1c660fdf310ef1ff
gencbirserhat <78283289+gencbirserhat@users.noreply.github.com>
2022-11-02 14:45:54 +0300

    rework

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { employee } from "./data/data";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Employees ({employee.length})</h1>
+      {employee.map((emp) => (
+        <p>{emp}</p>
+      ))}
     </div>
   );
 }
---
+++ src/data/data.js
@@ -0,0 +1,8 @@
+export const employee = [
+  "Serhat Genç",
+  "Mehmet Genç",
+  "Ayşe Genç",
+  "Ferhat Genç",
+  "Seda Genç",
+  "Metin Genç",
+];
